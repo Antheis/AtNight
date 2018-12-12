6f4b7d5413f084ab8ade7d2e004c2015b3bb8801
Timothée Puentes <timothee.puentes@epitech.eu>
2018-12-12 18:29:13 +0900

    Merge

---
+++ Assets/Scripts/MouseStartState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MouseStartState : MonoBehaviour
+{
+    public bool CursorIsVisible;
+    
+    private void Awake()
+    {
+        Cursor.visible = CursorIsVisible;
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Sound
+{
+    public class MusicManager : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        
+        private void Awake()
+        {
+            _audioSource = GetComponentInChildren<AudioSource>();
+        }
+
+        private void PlayMusic()
+        {
+            
+        }
+    }
+}
